df5f097bd15d54ab0509a44258c397f6e77e51ce
hiiragi-kaede <zoro18782ryt131@gmail.com>
2020-09-18 16:43:43 +0900

    ヘッダーの背景色と文字色を変更

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,5 +1,25 @@
+<style>
+    .myheader{
+        background-color: aqua;
+    }
+    .headertext{
+        color:white;
+         text-shadow:  2px  2px 5px #777 ,
+               -2px  2px 5px #777 ,
+                2px -2px 5px #777 ,
+               -2px -2px 5px #777;
+    }
+</style>
+
 <template>
-    <div>
-        ここはヘッダーです。
+    <div class="myheader">
+        <p class="headertext">
+            aiueo<br>
+            kakikukeko<br>
+            sasisuseso<br>
+            tatituteto<br>
+            naninuneno
+        </p>
     </div>
 </template>
+
