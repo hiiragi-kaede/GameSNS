1fe6b8274ae7ea27464845ce95a1a314ef1e1f3b
hiiragi <zoro18782ryt131@gmail.com>
2020-09-20 15:38:40 +0900

    ボタンでヘッダーの表示を変更する機能を作成

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -13,13 +13,33 @@
 
 <template>
     <div class="myheader">
-        <p class="headertext">
-            aiueo<br>
-            kakikukeko<br>
-            sasisuseso<br>
-            tatituteto<br>
-            naninuneno
+        <p class="headertext" id="headertext">
         </p>
+        <button v-if="b_state" @click="show" height="10">show</button>
+        <button v-else @click="hide" height="10">hide</button>
     </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+        b_state:true
+    }
+  },
+  methods:{
+    show(){
+        var h="aiueo<br>完成形";
+        document.getElementById('headertext').innerHTML=h;
+        this.b_state=false;
+    },
+    hide(){
+        var h="";
+        document.getElementById('headertext').innerHTML=h;
+        this.b_state=true;
+    }
+  }  
+}
+</script>
+
 
