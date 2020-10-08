1a1b06d8b7eb9f5758ce968b29b7e8d5c97149cf
hiiragi-kaede <zoro18782ryt131@gmail.com>
2020-10-08 21:59:41 +0900

    Revert "Merge branch 'feature_tutorial' of https://github.com/hiiragi-kaede/GameSNS into feature_tutorial"
    This reverts commit 9db1779c0dee17ae1daa205c2534174eb90e4be4, reversing
    changes made to b94d9b50cdef94048cfb9683d63df140243e8265.

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -32,33 +32,12 @@
 
 <template>
     <div class="myheader">
-        <p class="headertext" id="headertext">
-        </p>
-        <button v-if="b_state" @click="show" height="10">show</button>
-        <button v-else @click="hide" height="10">hide</button>
+        <ul class="menu">
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">USER</a></li>
+            <li><a href="#">SEARCH</a></li>
+            <li><a href="#">LOGOUT</a></li>
+        </ul>
     </div>
 </template>
-
-<script>
-export default {
-  data(){
-    return{
-        b_state:true
-    }
-  },
-  methods:{
-    show(){
-        var h="aiueo<br>完成形";
-        document.getElementById('headertext').innerHTML=h;
-        this.b_state=false;
-    },
-    hide(){
-        var h="";
-        document.getElementById('headertext').innerHTML=h;
-        this.b_state=true;
-    }
-  }  
-}
-</script>
-
 
